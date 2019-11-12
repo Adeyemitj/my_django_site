93a2d76be224a07360e9f0cde74a961996f6c9a3
Taiwo <adetwins2005@yahoo.com>
2019-11-12 17:24:48 +0100

    Show all blog posts on frontend

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from . models import Post
 
 # Create your views here.
+
+# create view for post_list.html
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[:5]
+    stuff_for_frontend = {'posts': posts} #creating context value
+    return render(request, 'blog/post_list.html', stuff_for_frontend)
