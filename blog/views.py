45d52f7f7e2018678af41055445da34fa1931cb2
Taiwo <adetwins2005@yahoo.com>
2019-11-15 09:50:02 +0100

    Add features to edit and list post from the frontend

--- blog/views.py
+++ blog/views.py
@@ -6,22 +6,24 @@ from .models import Post
 
 # Create your views here.
 
-# create view for post_list.html
+# create a function to view all posts
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[:5]
     stuff_for_frontend = {'posts': posts} #creating context value
     return render(request, 'blog/post_list.html', stuff_for_frontend)
 
+# create a function to display post detail
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     stuff_for_frontend = {'post': post}
     return render(request, 'blog/post_detail.html', stuff_for_frontend)
 
+# create a function to create new post
 def post_new(request):
     # for form submission. create form, save and redirect to post_detail.html page. (Post request)
     if request.method == 'POST':
         form = PostForm(request.POST)
-        if form.is_valid(): # check if form contents are valid
+        if form.is_valid():   # check if form contents are valid
             post = form.save(commit=False)  # save the new post but not committed to the database
             post.author = request.user      # take the user that login
             post.published_date = timezone.now()
@@ -32,3 +34,24 @@ def post_new(request):
         form = PostForm()
         stuff_for_frontend = {'form': form}
         return render(request, 'blog/post_edit.html', stuff_for_frontend)
+
+# create a function to edit post
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        # updating an existing form
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        # show post edit form at the frontend
+        form = PostForm(instance=post)
+        stuff_for_frontend = {'form': form}
+    return render(request, 'blog/post_edit.html', stuff_for_frontend)
+
+
+
